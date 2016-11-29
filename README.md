e055ea154145e6021343c1b2397b649b1401fc1b
Walter Mattingly <waltdakind@gmail.com>
2016-11-29 02:34:56 -0500

    basic setup, switch case

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # 501darts
-Interactive listing of outs in '01 based dart games.
+Calculate outs for a given number in '01 based dart games.
 Display the "outs" (number combination to reach zero) for a given number based on doubling out 
-(also calculate based on triple out possibility). 
+(also calculate based on triple out possibility, for which there aren't many out charts readily available).
+
+//TODO add option for no double bullseye
+
+
+Motivation for this is that most out charts don't display well on my phone and don't include tripling out options. Seems like a good exercise in using some of the ES2015 syntax.
+
+
 
--- dartOuts.js
+++ dartOuts.js
@@ -1,8 +1,14 @@
 
+// array to include all spots on the board sans multiplier
 let singles =[];
+// array of values on the outer ring of the board plus the double bullseye
 let doubles = [];
+// values of targets on inner ring midway between doubles ring and bullseye
 let triples = [];
-//adding all numbers except bulls to singles array
+// this will be either an array of arrays, or a single array
+let outsToShow = [];
+
+//adding all numbers except bullseye to singles array
 for(let i=1; i<21; i++){
 	singles.push(i);
 }
@@ -18,7 +24,7 @@ triples.push(num*3);
 
 // create triples array
 tripleOuts(singles);
-//adding bulls after creating triples array since it can be doubled but not tripled
+//adding bullseye after creating triples array since it can be doubled but not tripled
 singles.push(25);
 //singles array can now be used as outs when there is no double out
 
@@ -30,7 +36,58 @@ singles.push(25);
 let doubleOuts = (arr) => {
      singles.map(doubling); 
 }
-
 doubleOuts(singles);
+//console.log({"singles:":singles, "doubles: ":doubles, "triples: " :triples});
 
-console.log({"singles:":singles, "doubles: ":doubles, "triples: " :triples});
+// single array of all possible targets for use with map function
+let allTargets = [...singles, ...doubles, ...triples];
+//console.log(allTargets);
+
+// find out function  
+// don't just calculate out -- show best options as recommended
+// 
+
+let findOut = (num) => {
+
+
+}
+
+// step 1
+// for numbers 170 - 136 most outs use T20 and all require at least a T17
+// 164 154 146 -- a triple 19 is a viable option
+// 
+// 3 Dart out shots 135 - 101 
+//
+// 3 Dart out shots 80 - 61
+let getToAnOut = (num) => {
+switch(num) {
+    case num>180:
+        console.log("No outs available over 180.");
+        break;
+    case num=180:
+    //triple out only
+        outsToShow.push(["T20", "T20", "T20"]);
+        console.log(outsToShow);
+        break;
+    case (num>171 && num<180):
+
+    // first push T20 to outsToShow and test for T19, T18, T17
+//171 is 3 x T19
+//triple out only
+    case (num=171):
+        outsToShow.push(["T19", "T19", "T19"],["T20", "T19", "T18"]);
+        console.log(outsToShow);
+        break;
+    case (num>135 && num<171):
+//164 -- T20 - T18 - D25
+
+outsToShow.push(["T20"];
+        console.log(outsToShow);
+        break;
+    case (num>100 && num<136):
+        console.log();
+        break;
+    default:
+        console.log();
+}
+}
