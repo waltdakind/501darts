3bb35d1eff986131be7d09cad928f4aa56692750
Walter Mattingly <waltdakind@gmail.com>
2016-12-01 00:53:58 -0500

    express/server public folder

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express = require('express');
+const lookup = require('./lookup');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Define the port to run on
+app.set('PORT', PORT);
+
+//serve public folder
+app.use(express.static(__dirname + '/public'));
+
+// Listen for requests
+var server = app.listen(app.get('PORT'), function() {
+  console.log('Dayummmm that PORT is sooooo...' + PORT);
+});
